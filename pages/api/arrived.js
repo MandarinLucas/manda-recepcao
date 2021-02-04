bde80b36d5be1bb6d624212cb636eeadebbc20a1
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2021-02-04 15:33:36 -0300

    mandrill integration

--- pages/api/arrived.js
+++ pages/api/arrived.js
@@ -1,23 +1,24 @@
-import { PrismaClient } from "@prisma/client";
-import moment from "moment";
-import mandrill from "mandrill-api";
+import { PrismaClient } from '@prisma/client';
+import moment from 'moment';
+import mandrill from 'mandrill-api';
 
-import htmlTemplateGenerator from "../../utils/message-template";
+import htmlTemplateGenerator from '../../utils/message-template';
 
 const mandrillClient = new mandrill.Mandrill(process.env.MANDRILL_KEY);
 
 const prisma = new PrismaClient();
 
-export const arrived = async (req, res) => {
-  if (req.method !== "POST") {
+const arrived = async (req, res) => {
+  if (req.method !== 'POST') {
     res.statusCode = 405;
     res.json({ error: `This endpoint do not receive ${req.method} request` });
     return res;
   }
 
   const { nome, email, perfil, celular, aceite, quemvisitou } = req.body;
+  console.log(req.body);
 
-  let quemVisitouEmail = "";
+  let quemVisitouEmail = '';
   try {
     await prisma.visitantes.create({
       data: {
@@ -27,20 +28,21 @@ export const arrived = async (req, res) => {
         celular,
         aceite,
         quemvisitou,
-        data: moment().format("YYYY-MM-DD"),
-        hora: moment().format("hh:mm:ss"),
-      },
+        data: new Date(),
+        hora: new Date()
+      }
     });
 
-    const user = await prisma.usuarios.findUnique({
+    const user = await prisma.usuarios.findFirst({
       where: {
-        nome_usuario: quemvisitou,
-      },
+        nome_usuario: quemvisitou
+      }
     });
 
     quemVisitouEmail = user.email_usuario;
+    console.log(quemVisitouEmail);
   } catch (err) {
-    res.statusCode = 405; //verificar código
+    res.statusCode = 400;
     res.json({ error: `Error to record data to database - ${err}` });
     console.log(err);
     return res;
@@ -51,40 +53,46 @@ export const arrived = async (req, res) => {
 
   const message = {
     html: htmlMessage,
+    text: "Olá Mundo",
     subject,
-    from_email: "apoio@mandarin.com.br",
-    from_name: "Apoio Mandarin - Recepção",
+    from_email: 'apoio@mandarin.com.br',
+    from_name: 'Apoio Mandarin - Recepção',
     to: [
       {
-        email: "luiz.verissimo@mandarin.com.br",
-        name: "Luiz Fernando Verissimo",
-        type: "to",
+        email: 'luizfverissimo@gmail.com',
+        name: 'Luiz Fernando Verissimo',
+        type: 'to'
       },
-    ],
+    ]
   };
-
+  
   const async = false;
-  const ip_pool = "Main Pool";
-  const send_at = "example send_at";
+  const ip_pool = 'Main Pool';
+  const send_at = new Date();
+
+  const messageJSON = [JSON.stringify(message)]
 
   mandrillClient.messages.send(
     {
-      message: JSON.stringify(message),
-      async: async,
-      ip_pool: ip_pool,
-      send_at: send_at,
+      "message": messageJSON,
+      "async": async,
+      "ip_pool": ip_pool,
+      "send_at": send_at
     },
-    (res) => {
-      console.log(res);
-      res.statusCode = 200;
+    function(result) {
+      console.log("result", result);
+      res.statusCode = 201;
       res.json({ message: `Alerta enviado com sucesso.` });
       return res;
     },
-    (err) => {
-      res.statusCode = 405; //verificar código
+    function(err) {
+      res.statusCode = 400;
       res.json({ error: `Error to send e-mail - ${err}` });
       console.log(err);
       return res;
     }
   );
+  return
 };
+
+export default arrived;
